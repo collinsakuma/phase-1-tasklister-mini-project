c3f9f90cc86e4f35d88a1ed6854d74bd08d425e7
Collin Sakuma <collinmsakuma@gmail.com>
2023-02-01 18:53:25 -0800

    mini project complete]

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  document.querySelector('#create-task-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleToDo(e.target.new_task_description.value);
+  })
+
+  
+})
+
+function handleToDo(todo) {
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = 'Delete';
+  li.textContent = `${todo} `;
+  document.querySelector('#tasks').appendChild(li).append(btn);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
